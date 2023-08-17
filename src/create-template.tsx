10a34c09dc1bf394746c8b36ecfd789fb1a4bbcf
Raj <varasaniraj5@gmail.com>
2023-08-17 23:47:02 +0530

    create preseription

--- src/create-template.tsx
+++ src/create-template.tsx
@@ -1,99 +1,153 @@
 import React from "react";
 import ReactPDF, {
-    Page,
-    Text,
-    View,
-    Document,
-    StyleSheet,
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Image,
 } from "@react-pdf/renderer";
 
-type TemplateData = {
-    companyName: string;
-    companyPhone: string;
-    companyEmail: string;
-    receiptNumber: string;
-    datePaid: string,
-    paymentMethod: string,
-    amount: string
+type Medication = {
+  name: string;
+  dosage: {
+    morning: { beforeMeal: boolean; afterMeal: boolean };
+    afternoon: { beforeMeal: boolean; afterMeal: boolean };
+    night: { beforeMeal: boolean; afterMeal: boolean };
+  };
+};
+
+type Prescription = {
+  medications: Medication[];
+  advice: string;
+};
+
+type MedicalCaseData = {
+  patient: {
+    name: string;
+    email: string;
+    phone: string;
+  };
+  doctor: {
+    name: string;
+    email: string;
+    phone: string;
+  };
+  diagnosis: string[];
+  vitals: {
+    height: number;
+    weight: number;
+    pulse: number;
+    temperature: number;
+    bloodPressure: string;
+  };
+  complaints: {
+    description: string;
+    duration: number;
+    severity: string;
+    frequency: string;
+  }[];
+  prescription: Prescription;
+  completedAt: string;
 };
 
 interface PDFProps {
-    data: TemplateData;
+  data: MedicalCaseData;
 }
 
+const logoPlaceholder = "./Logo.png"; // Replace with actual logo URL
+
 const styles = StyleSheet.create({
-    page: {
-        backgroundColor: "#E4E4E4",
-    },
-    section: {
-        margin: 10,
-        padding: 10,
-    },
-    heading: {
-        fontSize: 24,
-        fontWeight: 600,
-        color: "#131925",
-        marginBottom: 8
-    },
-    statement: {
-        fontSize: 20,
-        color: "#131925",
-        lineHeight: 1.4,
-        marginBottom: 4,
-    },
-    divider: {
-        width: "100%",
-        height: 1,
-        backgroundColor: "#999999",
-        margin: "24px 0 24px 0"
-    },
-    paragraph: {
-        fontSize: 12,
-        color: "#212935",
-        lineHeight: 1.67,
-    },
-    columnParent: {
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
-    columnStart: {
-        flex: 1,
-    },
-    columnEnd: {
-        flex: 1,
-        alignItems: "flex-end"
-    },
+  page: {
+    backgroundColor: "#FFFFFF",
+    padding: 20,
+  },
+  logo: {
+    width: 147.22,
+    height: 25,
+    marginBottom: 20,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 600,
+    color: "#131925",
+    marginBottom: 10,
+  },
+  section: {
+    marginBottom: 20,
+  },
+  subheading: {
+    fontSize: 16,
+    fontWeight: 500,
+    color: "#131925",
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 12,
+    color: "#212935",
+    lineHeight: 1.67,
+  },
+  divider: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "#999999",
+    margin: "16px 0",
+  },
 });
 
 const PDF = ({ data }: PDFProps) => {
-    return (
-        <Document>
-            <Page size="A4" style={styles.page}>
-                <View style={styles.section}>
-                    <View style={styles.columnParent}>
-                        <View style={styles.columnStart}>
-                            <Text style={styles.heading}>{data.companyName}</Text>
-                            <Text style={styles.paragraph}>{data.companyPhone}</Text>
-                            <Text style={styles.paragraph}>{data.companyEmail}</Text>
-                        </View>
-                        <View style={styles.columnEnd}>
-                            <Text style={styles.heading}>Receipt</Text>
-                            <Text style={styles.paragraph}>Receipt number: {data.receiptNumber}</Text>
-                            <Text style={styles.paragraph}>Date paid: {data.datePaid}</Text>
-                            <Text style={styles.paragraph}>Payment method: {data.paymentMethod}</Text>
-                        </View>
-                    </View>
-                    <View style={styles.divider}></View>
-                    <View>
-                        <Text style={styles.statement}>{`${data.amount} paid on ${data.datePaid}`}</Text>
-                        <Text style={styles.paragraph}>Thank you for your business!</Text>
-                    </View>
-                </View>
-            </Page>
-        </Document>
-    );
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <Image src={logoPlaceholder} style={styles.logo} />
+        <Text style={styles.heading}>Medical Case Report</Text>
+        <View style={styles.section}>
+          <Text style={styles.subheading}>Patient Information</Text>
+          <Text style={styles.text}>Name: {data.patient.name}</Text>
+          <Text style={styles.text}>Email: {data.patient.email}</Text>
+          <Text style={styles.text}>Phone: {data.patient.phone}</Text>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.subheading}>Doctor Information</Text>
+          <Text style={styles.text}>Name: {data.doctor.name}</Text>
+          <Text style={styles.text}>Email: {data.doctor.email}</Text>
+          <Text style={styles.text}>Phone: {data.doctor.phone}</Text>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.subheading}>Complaints</Text>
+          {data.complaints.map((complaint, index) => (
+            <Text key={index} style={styles.text}>
+              {`${index + 1}. ${complaint.description}, Duration: ${
+                complaint.duration
+              } days, Severity: ${complaint.severity}, Frequency: ${
+                complaint.frequency
+              }`}
+            </Text>
+          ))}
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.subheading}>Diagnosis</Text>
+          {data.diagnosis.map((item, index) => (
+            <Text key={index} style={styles.text}>
+              {`${index + 1}. ${item}`}
+            </Text>
+          ))}
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.subheading}>Prescription</Text>
+          <Text style={styles.text}>Medications:</Text>
+          {data.prescription.medications.map((medication, index) => (
+            <Text key={index} style={styles.text}>
+              {`${index + 1}. ${medication.name}`}
+            </Text>
+          ))}
+          <Text style={styles.text}>Advice: {data.prescription.advice}</Text>
+        </View>
+      </Page>
+    </Document>
+  );
 };
 
-export default async (data: TemplateData) => {
-    return await ReactPDF.renderToStream(<PDF {...{ data }} />);
+export default async (data: MedicalCaseData) => {
+  return await ReactPDF.renderToStream(<PDF {...{ data }} />);
 };
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,80 @@ app.get("/", (req, res) => {
 
 app.post("/", async (req, res) => {
   // Calling the template render func with dynamic data
-  const result = await createTemplate(req.body);
+  const result = await createTemplate({
+    diagnosis: ["fever", "cough", "cold"],
+    patient: {
+      email: "imt_2020111@iiitm.ac.in",
+      name: "raj varsani",
+      phone: "1234567890",
+    },
+    doctor: {
+      email: "doctor@iiitm.ac.in",
+      name: "dr. prashant jain",
+      phone: "1234567890",
+    },
+    vitals: {
+      height: 167,
+      weight: 87,
+      pulse: 124,
+      temperature: 103,
+      bloodPressure: "120/80",
+    },
+    complaints: [
+      {
+        description: "weakness",
+        duration: 5,
+        severity: "high",
+        frequency: "constant",
+      },
+      {
+        description: "headache",
+        duration: 5,
+        severity: "high",
+        frequency: "constant",
+      },
+    ],
+    prescription: {
+      medications: [
+        {
+          dosage: {
+            morning: {
+              beforeMeal: true,
+              afterMeal: false,
+            },
+            afternoon: {
+              beforeMeal: true,
+              afterMeal: false,
+            },
+            night: {
+              beforeMeal: true,
+              afterMeal: true,
+            },
+          },
+          name: "Amoxicillin - 500 mg",
+        },
+        {
+          dosage: {
+            morning: {
+              beforeMeal: true,
+              afterMeal: false,
+            },
+            afternoon: {
+              beforeMeal: true,
+              afterMeal: false,
+            },
+            night: {
+              beforeMeal: true,
+              afterMeal: false,
+            },
+          },
+          name: "Amoxicillin - 500 mg",
+        },
+      ],
+      advice: "Take Bed Rest for two days",
+    },
+    completedAt: "2023-08-17T13:06:54.709Z",
+  });
 
   // Setting up the response headers
   res.setHeader("Content-Type", "application/pdf");
